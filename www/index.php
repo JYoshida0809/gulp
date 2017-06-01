07db4df6b2ab3ffba849b33825b4fe514f68901a
j.yoshida <yoshida@n-e-u.co.jp>
2017-06-01 09:56:26 +0900

    no message

--- www/index.php
+++ www/index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/common.css">
-</head>
-<body>
-
-  <p>gulpTest</p>
-  <p class="test">gulpTest</p>
-
-
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-  <script src="js/script.js"></script>
-
-
-</body>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="content">
+      <h1>helloWorld!</h1>
+      <p>テスト</p>
+      <h2>h2タイトル</h2>
+      <p class="test">テスト</p>
+      <h3>h3タイトル</h3>
+      <p>テスト</p>
+      <h4>h4タイトル</h4>
+      <p>テスト</p>
+      <h5>h5タイトル</h5>
+      <p>テスト</p>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
 </html>
